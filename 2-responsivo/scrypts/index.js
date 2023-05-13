32ac9414a35fad2a0a097e344b4f456ce0a6c063
Ícaro Baliza <icaroabf2002@gmail.com>
2023-05-13 02:10:36 -0300

    Adicionada funcionalidade de abrir e fechar menu

---
+++ 2-responsivo/scrypts/index.js
@@ -0,0 +1,10 @@
+function clickShare() {
+    const menu = document.querySelector(".menu")
+
+    if (menu.style.display == 'flex') {
+        menu.style.display = 'none'
+    }
+    else {
+        menu.style.display = 'flex'
+    }
+}

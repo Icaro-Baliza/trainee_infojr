8dbb979aed6a6b4eeda68a94e3d3e4851b2f58da
Ícaro Baliza <icaroabf2002@gmail.com>
2023-05-25 13:46:37 -0300

    ideia inicial

--- 4-atividade/jogo-da-velha/src/App.tsx
+++ 4-atividade/jogo-da-velha/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css"
-
+import Jogo from "./components/Jogo"
 
 function App() {
   return (
     <div className="App">
-
+      <h2>Jogo da Velha</h2>
+      <Jogo />
     </div>
   );
 }

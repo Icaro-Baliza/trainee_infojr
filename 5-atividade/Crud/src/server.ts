968890232eddccdfcff9201357e76afc333a7579
Ícaro Baliza <icaroabf2002@gmail.com>
2023-06-02 22:12:28 -0300

    Rotas criadas

--- 5-atividade/Crud/src/server.ts
+++ 5-atividade/Crud/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-
+import {router} from './routes';
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.json('Hello World!');
-});
+app.use(express.json())
+app.use(router)
+
+//app.get('/users', (request, response) => {
+  //return response.json('Hello World!');
+//});
 
 app.listen(3333, () => {
   console.log('HTTP Server running!');

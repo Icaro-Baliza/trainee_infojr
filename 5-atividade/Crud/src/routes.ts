74b522f3673bcf188b26d71f1fd8b632042e45f1
Ícaro Baliza <icaroabf2002@gmail.com>
2023-06-02 22:12:06 -0300

    Adicionada funcionalidade de criar usuario

--- 5-atividade/Crud/src/routes.ts
+++ 5-atividade/Crud/src/routes.ts
@@ -1,3 +1,35 @@
 import {Router} from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const router = Router();
+
+router.post("/cadastro", async (req, res) => {
+    try{
+        const {nome, email, idade, estado, cidade} = req.body;
+
+        let user = await prisma.user.findUnique({where: {email}})
+
+        if(user) {
+            return res.json({error: "Email já cadastrado"})
+        }
+
+        user = await prisma.user.create({
+            data: {
+                nome,
+                email,
+                idade,
+                estado,
+                cidade,
+            },
+        });
+        return res.json(user)
+    }
+    catch (error){
+        return res.json({error})
+    }
+})
+
+
+export{router};

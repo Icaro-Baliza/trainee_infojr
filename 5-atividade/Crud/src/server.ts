892a6458213c1cb95f49b796a9a2e2cfecbce0b3
Ícaro Baliza <icaroabf2002@gmail.com>
2023-06-03 00:11:24 -0300

    Projeto finalizado

--- 5-atividade/Crud/src/server.ts
+++ 5-atividade/Crud/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
 import {router} from './routes';
+
 const app = express();
 
 app.use(express.json())
 app.use(router)
-
-//app.get('/users', (request, response) => {
-  //return response.json('Hello World!');
-//});
 
 app.listen(3333, () => {
   console.log('HTTP Server running!');
